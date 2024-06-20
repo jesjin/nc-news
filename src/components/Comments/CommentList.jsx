b00f8ebb101f12aa35984f757125e1dd59c3d7d8
jesjin <jess.hk.jin@gmail.com>
2024-06-20 14:53:55 +0100

    Task 09 Delete comments - enabled delete comment, let user know the comment has been deleted. Apply setTimeout to clear messag e after 2 seconds

--- src/components/Comments/CommentList.jsx
+++ src/components/Comments/CommentList.jsx
@@ -1,45 +1,59 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import CommentCard from './CommentCard';
-import './CommentList.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CommentCard from "./CommentCard";
+import "./CommentList.css";
 
-const CommentList = ({articleId, initialComments=[]}) => {
-    const [loading, setLoading] = useState(initialComments.length);
-    const [error, setError] = useState(null);
-    const [comments, setComments] = useState(initialComments)
+const CommentList = ({ articleId, initialComments = [] }) => {
+  const [loading, setLoading] = useState(initialComments.length);
+  const [error, setError] = useState(null);
+  const [comments, setComments] = useState(initialComments);
+  const [deleteMessage, SetDeleteMessage] = useState(null);
 
-
-    useEffect(() => {
-      if (!comments.length) {
-        axios.get(`https://jesjin-nc-news.onrender.com/api/articles/${articleId}/comments`)
-          .then(response => {
-            setComments(response.data.comments);
-            setLoading(false);
-          })
-          .catch(error => {
-            setError('Error fetching comments');
-            setLoading(false);
-          });
-      } else {
-        setLoading(false);
-      }
-    }, [articleId, comments]);
-  
-    if (loading) {
-      return <p>Loading comments...</p>;
+  useEffect(() => {
+    if (!comments.length) {
+      axios
+        .get(
+          `https://jesjin-nc-news.onrender.com/api/articles/${articleId}/comments`
+        )
+        .then((response) => {
+          setComments(response.data.comments);
+          setLoading(false);
+        })
+        .catch((error) => {
+          setError("Error fetching comments");
+          setLoading(false);
+        });
+    } else {
+      setLoading(false);
     }
-  
-    if (error) {
-      return <p>{error}</p>;
-    }
-  
-    return (
-      <div className="comment-list">
-        {comments.map(comment => (
-          <CommentCard key={comment.comment_id} comment={comment} />
-        ))}
-      </div>
-    );
-  };  
+  }, [articleId, comments]);
+
+  const handleDelte = (commentId) => {
+    setComments(comments.filter((comment) => comment.comment_id !== commentId));
+    SetDeleteMessage('Comment successfully deleted.');
+    setTimeout(() => SetDeleteMessage(null), 2000);
+  };
+
+  if (loading) {
+    return <p>Loading comments...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  return (
+    <div className="comment-list">
+      {deleteMessage && <p className="delete-message">{deleteMessage}</p>}
+      {comments.map((comment) => (
+        <CommentCard
+          key={comment.comment_id}
+          comment={comment}
+          onDelete={handleDelte}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default CommentList;
